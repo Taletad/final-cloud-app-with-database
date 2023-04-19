fc4fed8d17989a8b8e03b498ce98884a8ce9ad33
Taddeo Capdeboscq <tadcap@gmail.com>
2023-04-19 09:53:41 +0200

    wip

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -95,6 +95,17 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
+#  <HINT> Create a Choice Model with:
+    # Used to persist choice content for a question
+    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
+    # Choice content
+    # Indicate if this choice of the question is a correct one or not
+    # Other fields and methods you would like to design
+class Choice(models.Model):
+    choice_content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
@@ -115,20 +126,25 @@ class Enrollment(models.Model):
     #    else:
     #        return False
 
+class Question(models.Model):
+    course = models.ManyToManyField(Course)
+    text_content = models.CharField(max_length=200)
+    grade_point = models.IntegerField(default=0)
+    choice_set = models.ManyToManyField(Choice)
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
-#  <HINT> Create a Choice Model with:
-    # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
-# class Choice(models.Model):
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
